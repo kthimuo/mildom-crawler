c9080def0c01e4628ccf27ec1b750a5ed154a271
kthimuo <hoge@example.com>
2021-03-04 04:03:45 +0900

    add InitilizerModel

--- poc/crawl_user_poc.py
+++ poc/crawl_user_poc.py
@@ -1,22 +1,27 @@
 import requests
 import json
 
-
-url = 'https://cloudac.mildom.com/nonolive/videocontent/profile/playbackList'
+url = 'https://cloudac.mildom.com/nonolive/gappserv/user/profileV2'
 
 user_id = '10105254'
-limit = '10000'
-page = '1'
 guest_id = 'pc-gp-68daaba9-d178-4e59-9868-e570116d26db'
+location= 'Japan|Mie'
+country = 'Japan'
+cluster='aws_japan&__platform=web&__la=ja'
+pcv='v2.10.25'
+sfr='pc'
+
 params = {
 'user_id' : user_id,
-'limit' : limit,
-'page' : page,
+'__platform':'web',
 }
 ua = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.192 Safari/537.36'
 headers = {
 	'user-agent': ua
 }
 res = requests.get(url,headers=headers,params=params).json()
-print(len(res['body']))
-print(res['body'][-1])
+print(res.keys())
+
+print(res['body'])
+
+
---
+++ poc/initializer_model.py
@@ -0,0 +1,6 @@
+class InitializerModel:
+    def __init__(self, arr=None):
+        """Array of initialization data"""
+        self._init(arr)
+    def _init(self, arr):
+        self._init_properties_custom(arr)
