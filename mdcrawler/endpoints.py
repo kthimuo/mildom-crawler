64a205a6dde43f6e8a3fc3790414ae1a75c93302
kthimuo <hoge@example.com>
2021-03-04 21:40:26 +0900

    add endpoints

---
+++ mdcrawler/endpoints.py
@@ -0,0 +1,4 @@
+PROFILE_URL = 'https://cloudac.mildom.com/nonolive/gappserv/user/profileV2'
+PLAYBACK_LIST_URL = 'https://cloudac.mildom.com/nonolive/videocontent/profile/playbackList'
+
+
--- mdcrawler/mildom.py
+++ mdcrawler/mildom.py
@@ -2,6 +2,7 @@ import requests
 import json
 from model.playback import PlayBack
 from model.user import User
+import endpoints
 
 class Mildom:
     def __init__(self):
@@ -9,27 +10,24 @@ class Mildom:
         self.user_agent =  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.192 Safari/537.36'
 
     def get_playbacks_by_uid(self, uid, count=5):
-        url = 'https://cloudac.mildom.com/nonolive/videocontent/profile/playbackList'
         params = {
         'user_id' : uid,
         'limit' : count,
         }
-
         headers = {
             'user-agent': self.user_agent
         }
-        res = self.__req.get(url,headers=headers,params=params).json()
-
+        res = self.__req.get(
+                endpoints.PLAYBACK_LIST_URL,
+                headers=headers,
+                params=params).json()
         playbacks = []
         for p in res['body']:
             playback = PlayBack(p)
             playbacks.append(playback)
-
         return playbacks
             
     def get_account_by_uid(self, uid):
-
-        url = 'https://cloudac.mildom.com/nonolive/gappserv/user/profileV2'
         params = {
         'user_id' : uid,
         '__platform':'web',
@@ -37,7 +35,9 @@ class Mildom:
         headers = {
             'user-agent': self.user_agent
         }
-        res = self.__req.get(url,headers=headers,params=params).json()
+        res = self.__req.get(endpoints.PROFILE_URL,
+                headers=headers,
+                params=params).json()
         user = User(res['body'])
         return user
 
