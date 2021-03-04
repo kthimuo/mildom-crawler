dbf00aa73ea8fdaee9d83971939e601cc8e3541b
kthimuo <hoge@example.com>
2021-03-05 03:51:12 +0900

    add get_chats_by_pbid

--- mdcrawler/mildom.py
+++ mdcrawler/mildom.py
@@ -2,12 +2,15 @@ import requests
 import json
 from model.playback import PlayBack
 from model.user import User
+from model.chat import Chat
 import endpoints
+import time
 
 class Mildom:
     def __init__(self):
         self.__req = requests
         self.user_agent =  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.192 Safari/537.36'
+        self.sleep_sec = 1.0
 
     def get_playbacks_by_uid(self, uid, count=5):
         params = {
@@ -41,15 +44,79 @@ class Mildom:
         user = User(res['body'])
         return user
 
+    def get_playback_lengh_by_pbid(self,pbid):
+        params = {
+        'v_id' : pbid,
+        '__platform':'web',
+        }
+        headers = {
+            'user-agent': self.user_agent
+        }
+        res = self.__req.get(endpoints.PLAYBACK_URL,
+                headers=headers,
+                params=params).json()
+        length = int(res['body']['playback']['video_length'])
+        return length
+
+    def get_chats_by_pbid(self, pbid, count=100):
+        '''
+        pbid -> PlayBackId
+        '''
+        length = self.get_playback_lengh_by_pbid(pbid)
+        time_offset_ms = 0
+        end_offset_ms = ''
+        end_offset_ms = ''
+        next_time = 0
+        chats = []
+        cnt = 0
+
+        while cnt <= count and next_time <= length :
+            params = {
+            'video_id' : pbid,
+            'time_offset_ms': time_offset_ms,
+            'end_offset_ms': end_offset_ms,
+            }
+            headers = {
+                'user-agent': self.user_agent,
+                'sec-fetch-dest': 'empty',
+                'sec-fetch-mode': 'cors',
+                'sec-fetch-site': 'cross-site'
+            }
+            res = requests.get(endpoints.CHAT_URL,headers=headers,params=params).json()
+            models = res["body"]["models"]
+            for i in range(len(models)):
+                detail = res["body"]["models"][i]["detail"]
+                for arr in detail:
+                    arr['pbid'] = pbid
+                    chat = Chat(arr)
+                    cnt +=1
+                    chats.append(chat)
+            next_time = res["body"]["models"][-1]["summary"]["end_offset_ms"]
+            time_offset_ms = next_time
+            time.sleep(self.sleep_sec)
+        return chats    
+
+
 
 if __name__ == '__main__':
+    import sys
+    from model.user import User
     uid = '10084097'
+    pbid = '10105254-1610795759'
     mildom = Mildom()
-    playbacks = mildom.get_playbacks_by_uid(uid,count=100)
-    user = mildom.get_account_by_uid(uid)
-    print(len(playbacks))
-    print(playbacks[-1])
-    print(user)
+
+#    user = mildom.get_account_by_uid(uid)
+#    playbacks = mildom.get_playbacks_by_uid(uid,count=100)
+#    playback = playbacks[0]
+#    pbid = playback.pbid
+#    chats = mildom.get_chats_by_pbid(pbid,count=1000)
+#    print(user)
+#    print('-----------')
+#    print(playback)
+#    print('-----------')
+#    print(chats[0])
+
+
     
 
 
