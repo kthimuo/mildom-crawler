c9b4f78d279e1466fbe4b69957ee58797033d485
kthimuo <hoge@example.com>
2021-03-05 04:12:44 +0900

    fix

--- mdcrawler/mildom.py
+++ mdcrawler/mildom.py
@@ -1,10 +1,10 @@
 import requests
 import json
-from model.playback import PlayBack
-from model.user import User
-from model.chat import Chat
-import endpoints
 import time
+from .model.playback import PlayBack
+from .model.user import User
+from .model.chat import Chat
+from . import endpoints
 
 class Mildom:
     def __init__(self):
@@ -58,10 +58,15 @@ class Mildom:
         length = int(res['body']['playback']['video_length'])
         return length
 
-    def get_chats_by_pbid(self, pbid, count=100):
+    def get_chats_by_pbid(self, pbid, count=None):
         '''
         pbid -> PlayBackId
         '''
+        if not count:
+            index = float('inf')
+        else :
+            index = count
+
         length = self.get_playback_lengh_by_pbid(pbid)
         time_offset_ms = 0
         end_offset_ms = ''
@@ -70,7 +75,7 @@ class Mildom:
         chats = []
         cnt = 0
 
-        while cnt <= count and next_time <= length :
+        while cnt <= index and next_time <= length :
             params = {
             'video_id' : pbid,
             'time_offset_ms': time_offset_ms,
@@ -94,30 +99,9 @@ class Mildom:
             next_time = res["body"]["models"][-1]["summary"]["end_offset_ms"]
             time_offset_ms = next_time
             time.sleep(self.sleep_sec)
+        if  count:
+            chats = chats[:count]
         return chats    
-
-
-
-if __name__ == '__main__':
-    import sys
-    from model.user import User
-    uid = '10084097'
-    pbid = '10105254-1610795759'
-    mildom = Mildom()
-
-#    user = mildom.get_account_by_uid(uid)
-#    playbacks = mildom.get_playbacks_by_uid(uid,count=100)
-#    playback = playbacks[0]
-#    pbid = playback.pbid
-#    chats = mildom.get_chats_by_pbid(pbid,count=1000)
-#    print(user)
-#    print('-----------')
-#    print(playback)
-#    print('-----------')
-#    print(chats[0])
-
-
-    
 
 
         
