8c6b8f54c964e0a2aa4188081c9dd71d100da41f
kthimuo <hoge@example.com>
2021-03-05 00:39:00 +0900

    fix

--- mdcrawler/model/chat.py
+++ mdcrawler/model/chat.py
@@ -1,19 +1,23 @@
 from .user import User
 import textwrap
+import json
 
 class Chat(User):
     def __init__(self,arr):
         if arr:
+            arr['message'] = json.loads(arr['message'])
             super()._init_properties_custom(arr,prop='chat')
             self._init_properties_custom(arr)
 
     def _init_properties_custom(self, arr):
-        chat_text = arr['msg']
-        chat_id = arr['msgId']
-        chat_time = int(int(arr['time'])/1000)
+        chat_text = arr['message']['msg']
+        chat_id = arr['message']['msgId']
+        chat_time = int(int(arr['message']['time'])/1000)
+        chat_time_offset = int(int(arr['time_offset_ms'])/1000)
         self.chat_text = chat_text
         self.chat_id = chat_id
         self.chat_time = chat_time
+        self.chat_time_offset = chat_time_offset
 
     def __str__(self):
         string = f"""
@@ -21,6 +25,7 @@ class Chat(User):
         ChatId: {self.chat_id if hasattr(self, 'chat_id') else '-'}
         ChatText: {self.chat_text if hasattr(self, 'chat_text') else '-'}
         ChatTime: {self.chat_time if hasattr(self, 'chat_time') else '-'}
+        ChatTimeOffset: {self.chat_time_offset if hasattr(self, 'chat_time_offset') else '-'}
         Username: {self.username if hasattr(self, 'username') else '-'}
         UserId: {self.user_id if hasattr(self, 'user_id') else '-'}
         Level: {self.level if hasattr(self, 'level') else '-'}
