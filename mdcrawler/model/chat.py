d90c04b2c94d36fedbbac2df918f4a92fad63c1f
kthimuo <hoge@example.com>
2021-03-11 02:46:21 +0900

    fix model

--- mdcrawler/model/chat.py
+++ mdcrawler/model/chat.py
@@ -1,28 +1,32 @@
 from .user import User
 from .playback import PlayBack
+from .room import Room
 import textwrap
 import json
 
-class Chat(PlayBack):
-    def __init__(self,arr=None):
+class PlayBackChat(PlayBack):
+    def __init__(self,arr=None, prop=None):
+        if not prop:
+            prop = 'playback_chat'
         if arr:
             arr['message'] = json.loads(arr['message'])
-            super(Chat,self).__init__(arr,prop='chat')
-            super(Chat,self)._init_properties_custom(arr,prop='chat')
+            super(PlayBackChat,self).__init__(arr,prop=prop)
+            super(PlayBackChat,self)._init_properties_custom(arr,prop=prop)
 
     def _init_properties_custom(self, arr,prop=None):
-        chat_text = arr['message']['msg']
-        chat_id = arr['message']['msgId']
-        chat_time = int(int(arr['message']['time'])/1000)
-        chat_time_offset = int(int(arr['time_offset_ms'])/1000)
-        self.chat_text = chat_text
-        self.chat_id = chat_id
-        self.chat_time = chat_time
-        self.chat_time_offset = chat_time_offset
+        if prop == 'playback_chat':
+            chat_text = arr['message']['msg']
+            chat_id = arr['message']['msgId']
+            chat_time = int(int(arr['message']['time'])/1000)
+            chat_time_offset = int(int(arr['time_offset_ms'])/1000)
+            self.chat_text = chat_text
+            self.chat_id = chat_id
+            self.chat_time = chat_time
+            self.chat_time_offset = chat_time_offset
 
     def __str__(self):
         string = f"""
-        Chat info:
+        PlayBackChat info:
         ChatId: {self.chat_id if hasattr(self, 'chat_id') else '-'}
         ChatText: {self.chat_text if hasattr(self, 'chat_text') else '-'}
         ChatTime: {self.chat_time if hasattr(self, 'chat_time') else '-'}
@@ -30,6 +34,37 @@ class Chat(PlayBack):
         Username: {self.username if hasattr(self, 'username') else '-'}
         UserId: {self.user_id if hasattr(self, 'user_id') else '-'}
         PlayBackId: {self.pbid if hasattr(self, 'pbid') else '-'}
+        """
+
+        return textwrap.dedent(string)
+
+
+class RoomChat(Room):
+    def __init__(self,arr=None, prop=None):
+        if not prop:
+            prop = 'room_chat'
+        if arr:
+            super(RoomChat,self).__init__(arr,prop=prop)
+            super(RoomChat,self)._init_properties_custom(arr,prop=prop)
+
+    def _init_properties_custom(self, arr,prop=None):
+        if prop == 'room_chat':
+            chat_text = arr['msg']
+            chat_id = arr['msgId']
+            chat_time = int(int(arr['time'])/1000)
+            self.chat_text = chat_text
+            self.chat_id = chat_id
+            self.chat_time = chat_time
+
+    def __str__(self):
+        string = f"""
+        RoomChat info:
+        ChatId: {self.chat_id if hasattr(self, 'chat_id') else '-'}
+        ChatText: {self.chat_text if hasattr(self, 'chat_text') else '-'}
+        ChatTime: {self.chat_time if hasattr(self, 'chat_time') else '-'}
+        Username: {self.username if hasattr(self, 'username') else '-'}
+        UserId: {self.user_id if hasattr(self, 'user_id') else '-'}
+        RoomId: {self.room_id if hasattr(self, 'room_id') else '-'}
         """
         return textwrap.dedent(string)
 
--- mdcrawler/model/room.py
+++ mdcrawler/model/room.py
@@ -2,27 +2,30 @@ from .initializer_model import InitializerModel
 from .user import User
 import textwrap
 
-class Live(User):
+class Room(User):
     def __init__(self,arr=None,prop=None):
         if not prop:
-            prop = 'live'
+            prop = 'room'
         if arr:
-            super().__init__(arr,prop=prop)
-            super()._init_properties_custom(arr,prop=prop)
+            super(Room, self).__init__(arr,prop=prop)
+            super(Room, self)._init_properties_custom(arr,prop=prop)
 
 
     def _init_properties_custom(self, arr, prop):
-        if prop == 'live':
+        if prop == 'room':
             start_timestamp = int(int(arr['live_start_ms'])/1000)
             intro = arr['live_intro']
             title = arr['anchor_intro']
             self.start_timestamp = start_timestamp
             self.title = title
             self.intro = intro
+        elif prop == 'room_chat':
+            room_id = arr['roomId']
+            self.room_id = room_id
 
     def __str__(self):
         string = f"""
-        Live info:
+        Room info:
         Title: {self.title if hasattr(self, 'title') else '-'}
         Intro: {self.intro if hasattr(self, 'intro') else '-'}
         Start: {self.start_timestamp if hasattr(self, 'start_timestamp') else '-'}
