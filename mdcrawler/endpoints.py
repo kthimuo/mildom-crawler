6274d55c5509614f9ea454058658e2b44f2cbf22
tamura <thi@tamuranoMacBook-Pro.local>
2021-08-13 02:14:06 +0900

    fix playback endpoint

--- mdcrawler/endpoints.py
+++ mdcrawler/endpoints.py
@@ -1,5 +1,5 @@
 PROFILE_URL = 'https://cloudac.mildom.com/nonolive/gappserv/user/profileV2'
-PLAYBACK_LIST_URL = 'https://cloudac.mildom.com/nonolive/videocontent/profile/playbackList'
+PLAYBACK_LIST_URL = 'https://cloudac-cf-jp.mildom.com/nonolive/videocontent/profile/playbackList'
 
 CHAT_URL = 'https://cloudac.mildom.com/nonolive/videocontent/chat/replay'
 PLAYBACK_URL = 'https://cloudac.mildom.com/nonolive/videocontent/playback/getPlaybackDetail'
